6ab7ce803411015c883fe2597ca45cc2369678c4
Duy Nguyen <nguyenduy010798@gmail.com>
2021-04-17 18:41:49 +0300

    update script

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-pm2 stop mlem-mlem-backend
-pm2 delete mlem-mlem-backend
+pm2 stop mlem-mlem-backend || echo "already stopped"
+pm2 delete mlem-mlem-backend  || echo "already removed"
