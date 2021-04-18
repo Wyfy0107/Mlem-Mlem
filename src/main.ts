5407afaf8aa085c05b7b34f5e2668a5ea78e33d2
Duy Nguyen <nguyenduy010798@gmail.com>
2021-04-18 20:24:02 +0300

    increase urlenconded size

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
-import { json } from 'express'
+import { json, urlencoded } from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors()
   app.use(json({ limit: '4mb' }))
+  app.use(urlencoded({ limit: '4mb' }))
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
